6fe354562d4245c38f8ec3a5de0dd6c8506ee3b0
zekaeko.p <zekaeko.p@gmail.com>
2026-04-01 15:14:51 +0300

    added propper navbar

--- templates/base/navbar.php
+++ templates/base/navbar.php
@@ -2,10 +2,12 @@
     <div class="d-flex justify-content-center">
         <div class="card">
             <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Home</a></li>
-                <li><a href="">Home</a></li>
-                <li><a href="">Home</a></li>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="products.php">Products </a></li>
+                <?php if ($_SESSION["user"]["role"] == "admin"): ?>
+                    <li><a href="product_stocks.php">Product stocks</a></li>
+                    <li><a href="admin_panel.php">Admin Panel</a></li>
+                <?php endif ?>
             </ul>
         </div>
     </div>
