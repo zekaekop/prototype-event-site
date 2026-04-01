12667b07c0a1492e7e3ef81fec50a8af466e3907
zekaeko.p <zekaeko.p@gmail.com>
2026-04-01 15:16:02 +0300

    renamed the login register file foldert o account and fixed db.php to work

--- templates/base/db.php
+++ templates/base/db.php
@@ -3,11 +3,11 @@
 $host = "localhost";
 $dbname = "CAS";
 $user = "root";
-$pwd = "";
+$pwd = "eko";
 
 try{
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pwd);
-    $pdo -> Attribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e){
     die("LOST CONNECTION DB ". $e->getMessage());
 }
